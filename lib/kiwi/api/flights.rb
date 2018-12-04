04c12ba2196de7f26ebe65f1b19f8edccda084e8
Mikhail Slyusarev <slyusarevmikhail@gmail.com>
2018-12-03 22:05:20 -0800

    updating names to reflect changes in the api

--- lib/kiwi/api/flights.rb
+++ lib/kiwi/api/flights.rb
@@ -4,98 +4,98 @@ module Kiwi
     FLIGHTS_PARAM_CONVERTER = ParameterConverter.new do
       convert(:version) { |v| { v: v } }
       convert(:currency) { |v| { curr: v } }
-      convert(:from) { |v| { flyFrom: [*v].join(',') } }
-      convert(:to) { |v| { to: [*v].join(',') } }
-      convert(:depart) { |v| %I[dateFrom dateTo].zip(date_range(v)).to_h }
-      convert(:return) { |v| %I[returnFrom returnTo].zip(date_range(v)).to_h }
-      convert(:max_flight_time) { |v| { maxFlyDuration: v } }
-      convert(:unique_cities) { |v| { oneforcity: bool_to_int(v) } }
+      convert(:from) { |v| { fly_from: [*v].join(',') } }
+      convert(:to) { |v| { fly_to: [*v].join(',') } }
+      convert(:depart) { |v| %I[date_from date_to].zip(date_range(v)).to_h }
+      convert(:return) { |v| %I[return_from return_to].zip(date_range(v)).to_h }
+      convert(:max_flight_time) { |v| { max_fly_duration: v } }
+      convert(:unique_cities) { |v| { one_for_city: bool_to_int(v) } }
       convert(:unique_dates) { |v| { one_per_date: bool_to_int(v) } }
       convert(:passengers) { |v| v }
       convert(:price) { |v| %I[price_from price_to].zip(maybe_range(v)).to_h }
-      convert(:direct) { |v| { directFlights: bool_to_int(v) } }
-      convert(:max_stopovers) { |v| { maxstopovers: v } }
+      convert(:direct) { |v| { direct_flights: bool_to_int(v) } }
+      convert(:max_stopovers) { |v| { max_stopovers: v } }
       convert(:ascending) { |v| { asc: bool_to_int(v) } }
 
       convert(:depart_takeoff) do |v|
-        %I[dtimefrom dtimeto].zip(maybe_range(v)).to_h
+        %I[dtime_from dtime_to].zip(maybe_range(v)).to_h
       end
 
       convert(:depart_land) do |v|
-        %I[atimefrom atimeto].zip(maybe_range(v)).to_h
+        %I[atime_from atime_to].zip(maybe_range(v)).to_h
       end
 
       convert(:return_takeoff) do |v|
-        %I[returndtimefrom returndtimeto].zip(maybe_range(v)).to_h
+        %I[ret_dtime_from ret_dtime_to].zip(maybe_range(v)).to_h
       end
 
       convert(:return_land) do |v|
-        %I[returnatimefrom returnatimeto].zip(maybe_range(v)).to_h
+        %I[ret_atime_from ret_atime_to].zip(maybe_range(v)).to_h
       end
 
       convert(:nights) do |v|
-        %I[daysInDestinationFrom daysInDestinationTo].zip(maybe_range(v)).to_h
+        %I[nights_in_dst_from nights_in_dst_to].zip(maybe_range(v)).to_h
       end
 
       convert(:depart_days) do |v|
-        params = { flyDaysType: 'departure' }
+        params = { fly_days_type: 'departure' }
 
-        next params.merge(flyDays: [1, 2, 3, 4, 5]) if v == :weekdays
-        next params.merge(flyDays: [0, 6]) if v == :weekends
+        next params.merge(fly_days: [1, 2, 3, 4, 5]) if v == :weekdays
+        next params.merge(fly_days: [0, 6]) if v == :weekends
 
-        params.merge(flyDays: v.map(&day_number))
+        params.merge(fly_days: v.map(&day_number))
       end
 
       convert(:return_days) do |v|
-        params = { returnFlyDaysType: 'departure' }
+        params = { ret_fly_days_type: 'arrival' }
 
-        next params.merge(returnFlyDays: [1, 2, 3, 4, 5]) if v == :weekdays
-        next params.merge(returnFlyDays: [0, 6]) if v == :weekends
+        next params.merge(ret_fly_days: [1, 2, 3, 4, 5]) if v == :weekdays
+        next params.merge(ret_fly_days: [0, 6]) if v == :weekends
 
-        params.merge(returnFlyDays: v.map(&day_number))
+        params.merge(ret_fly_days: v.map(&day_number))
       end
 
       convert(:stopover_length) do |v|
-        %I[stopoverfrom stopoverto].zip(maybe_range(v)).to_h
+        %I[stopover_from stopover_to].zip(maybe_range(v)).to_h
       end
 
       convert(:multi_airport) do |v|
-        { connectionsOnDifferentAirport: bool_to_int(v) }
+        { conn_on_diff_airport: bool_to_int(v) }
       end
 
       convert(:return_from_same) do |v|
-        { returnFromDifferentAirport: bool_to_int(v) }
+        { ret_from_diff_airport: bool_to_int(v) }
       end
 
       convert(:return_to_same) do |v|
-        { returnToDifferentAirport: bool_to_int(v) }
+        { ret_to_diff_airport: bool_to_int(v) }
       end
 
       convert(:include_airlines) do |v|
         {
-          selectedAirlines: [*v].join(','),
-          selectedAirlinesExclude: 0
+          select_airlines: [*v].join(','),
+          select_airlines_exclude: 0
         }
       end
 
       convert(:exclude_airlines) do |v|
         {
           selectedAirlines: [*v].join(','),
-          selectedAirlinesExclude: 1
+          select_airlines_exclude: 1
         }
       end
 
       convert(:include_stopovers) do |v|
         {
-          selectedStopoverAirports: [*v].join(','),
-          selectedStopoverAirportsExclude: 0
+          select_stop_airport: [*v].join(','),
+          select_stop_airport_exclude: 0
         }
       end
 
       convert(:exclude_stopovers) do |v|
         {
-          selectedStopoverAirports: [*v].join(','),
-          selectedStopoverAirportsExclude: 1
+          select_stop_airport: [*v].join(','),
+          select_stop_airport_exclude: 1
         }
       end
     end
@@ -122,6 +122,8 @@ module Kiwi
       def search(params)
         request_params =
           @param_converter.process(@default_params.merge(params))
+
+        puts request_params.inspect
 
         response = @conn.get(PATH, request_params)
 
